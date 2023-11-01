7212a9df98a8c71ef2fd26ce4b78feedff091c38
Fikreselassie <101448903+zsegla@users.noreply.github.com>
2023-11-01 14:55:56 +0300

    Create two-sum.py

---
+++ two-sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/two-sum/
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:    
+        dic = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in dic:
+                return [dic[diff], i]
+            dic[num] = i
+        # it is guaranteed that there is a solution
